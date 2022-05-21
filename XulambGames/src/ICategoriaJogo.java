4026db3b20c75be09cb085370733d2b0608bbc3f
Eric Stefano <ericstefano12@gmail.com>
2022-05-21 17:58:47 -0300

    :sparkles:Comentários iniciais

--- XulambGames/src/ICategoriaJogo.java
+++ XulambGames/src/ICategoriaJogo.java
@@ -1,3 +1,9 @@
 public interface ICategoriaJogo {
+    /**
+     * Calcula o preço final do jogo baseado na regra da categoria
+     * 
+     * @param preco double preco original do jogo para ser calculado
+     * @return double preco final do jogo, após ser aplicado a regra da categoria
+     */
     public double precoVenda(double preco);
 }
