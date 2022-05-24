f2e376de8d3679e6de1380519fd4512e7944c3cd
hugogmartins <83436960+hugogmartins@users.noreply.github.com>
2022-05-23 21:23:35 -0300

    Cliente
    mudancas em cliente e suas herancas

--- XulambGames/src/Cliente.java
+++ XulambGames/src/Cliente.java
@@ -1,3 +1,69 @@
-public class Cliente {
+import java.util.LinkedList;
 
+public class Cliente{
+    private String nome;
+    private String usuario;
+    private String senha;
+    private ICategoriaCliente categoria;
+    private LinkedList<Pedido> pedidos;
+
+    public Cliente(String nome, String usuario, String senha){
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.categoria = null;
+        this.pedidos = new LinkedList<Pedido>();
+    }
+
+    public String historicoDeCompras(ICategoriaJogo categoria){
+        StringBuilder impressao = new StringBuilder();
+        return impressao.toString();
+    }
+
+    public String historicoDeCompras(){
+        StringBuilder impressao = new StringBuilder();
+        return impressao.toString();
+    }
+
+    public boolean mudarCategoria(ICategoriaCliente categoria) {
+        this.categoria = categoria;
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return ("*****USUARIO*****\nNome: " + this.nome + "\nUsuário: " + this.usuario);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public ICategoriaCliente getCategoria() {
+        return categoria;
+    }
+
+    public LinkedList<Pedido> getPedidos() {
+        return pedidos;
+    }
 }
